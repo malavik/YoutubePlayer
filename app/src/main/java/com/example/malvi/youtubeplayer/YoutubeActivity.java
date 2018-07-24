08173c6fa1c96bd1e7ad07bd42f6ec4b6d7d6e0e
malavika <malvi.malavika@gmail.com>
2018-07-24 16:53:00 -0400

    display error message on initialization failure

--- app/src/main/java/com/example/malvi/youtubeplayer/YoutubeActivity.java
+++ app/src/main/java/com/example/malvi/youtubeplayer/YoutubeActivity.java
@@ -4,6 +4,7 @@ import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
@@ -21,9 +22,10 @@ public class YoutubeActivity extends YouTubeBaseActivity implements YouTubePlaye
         ConstraintLayout cLayout = (ConstraintLayout) getLayoutInflater().inflate(R.layout.activity_youtube, null);
         setContentView(cLayout);
 
-        YouTubePlayerView ytPlayer = new YouTubePlayerView(this);
-        ytPlayer.setLayoutParams(new ConstraintLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
-        cLayout.addView(ytPlayer);
+        YouTubePlayerView tubePlayerView = new YouTubePlayerView(this);
+        tubePlayerView.setLayoutParams(new ConstraintLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        cLayout.addView(tubePlayerView);
+        tubePlayerView.initialize(GOOGLE_API_KEY,this);
     }
 
     @Override
@@ -33,6 +35,13 @@ public class YoutubeActivity extends YouTubeBaseActivity implements YouTubePlaye
 
     @Override
     public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+        final int REQUEST_CODE = 1;
 
+        if(youTubeInitializationResult.isUserRecoverableError()){
+            youTubeInitializationResult.getErrorDialog(this, REQUEST_CODE).show();
+        }else{
+            String errorMsg = String.format("YouTube player initialization error (%1$s)", youTubeInitializationResult.toString() );
+            Toast.makeText(this, errorMsg, Toast.LENGTH_SHORT).show();
+        }
     }
 }
