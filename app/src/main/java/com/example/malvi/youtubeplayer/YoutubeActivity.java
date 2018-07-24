dfe7094d75da97a00a410a0f19bb2e6d9e2047b4
malavika <malvi.malavika@gmail.com>
2018-07-24 14:42:24 -0400

    add youtubeplayer view to dynamic constraint layout

--- app/src/main/java/com/example/malvi/youtubeplayer/YoutubeActivity.java
+++ app/src/main/java/com/example/malvi/youtubeplayer/YoutubeActivity.java
@@ -3,10 +3,12 @@ package com.example.malvi.youtubeplayer;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
 
 import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
 
 public class YoutubeActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener{
 
@@ -15,6 +17,10 @@ public class YoutubeActivity extends YouTubeBaseActivity implements YouTubePlaye
         super.onCreate(savedInstanceState);
         ConstraintLayout cLayout = (ConstraintLayout) getLayoutInflater().inflate(R.layout.activity_youtube, null);
         setContentView(cLayout);
+
+        YouTubePlayerView ytPlayer = new YouTubePlayerView(this);
+        ytPlayer.setLayoutParams(new ConstraintLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        cLayout.addView(ytPlayer);
     }
 
     @Override
